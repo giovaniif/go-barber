48a7ab20af49a0f0dcfed6887d2090f1089dcd6f
Giovani Farias <riccog.25@gmail.com>
2020-04-16 17:35:21 -0300

    create AppError class

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
+import AppError from '../errors/AppError';
 
 import User from '../models/User';
 
@@ -18,7 +19,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('Email addres already used');
+      throw new AppError('Email address already used');
     }
 
     const hashedPassword = await hash(password, 8);
