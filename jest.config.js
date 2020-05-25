3806f1d88f99d58ad4a1e3f437421994b34b5742
Giovani Farias <riccog.25@gmail.com>
2020-05-25 10:37:50 -0300

    fix imports with jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig.json');
 
