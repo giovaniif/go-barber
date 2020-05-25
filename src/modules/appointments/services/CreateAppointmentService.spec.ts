77721fa47b772c01986c780413d914fae15b2fdc
Giovani Farias <riccog.25@gmail.com>
2020-05-25 10:40:34 -0300

    create appointment test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,11 +2,19 @@ import CreateAppointmentService from './CreateAppointmentService';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
+  it('should be able to create a new appointment', async () => {
     const fakeAppointmentsRepository = new FakeAppointmentsRepository();
     const createAppointment = new CreateAppointmentService(
       fakeAppointmentsRepository
     );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123123');
   });
 
   it('should not be able to create two appointments on the same time', () => {
